8e68ad605ef458e2e92db67a2c428a866a489e0e
DeepaliKosta <deepalikosta1234@gmail.com>
2022-03-14 18:00:58 +0530

    commit

--- SpringMVCdemo/src/main/java/com/capg/controller/HeyController.java
+++ SpringMVCdemo/src/main/java/com/capg/controller/HeyController.java
@@ -4,29 +4,30 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HeyController {
 
-//	@RequestMapping("/hey")
-//	public String display(HttpServletRequest request,Model m) {
-//		String name=request.getParameter("name");
-//		String password = request.getParameter("pass");
-//		
-//		if(password.equals("admin")) {
-//			String msg = "Hello " + name;
-//			m.addAttribute("message",msg);
-//			return "viewpage";
-//		}
-//		else
-//		{
-//			String msg="Sorry " + name + ". You entered an incorrect password";
-//			m.addAttribute("message", msg);
-//			return "errorpage";
-//		
-//		}
-//	}
+	@RequestMapping("/hey")
+	public String display(HttpServletRequest request,Model m) {
+		String name=request.getParameter("name");
+		String password = request.getParameter("pass");
+		
+		if(password.equals("admin")) {
+			String msg = "Hello " + name;
+			m.addAttribute("message",msg);
+			return "viewpage";
+		}
+		else
+		{
+			String msg="Sorry " + name + ". You entered an incorrect password";
+			m.addAttribute("message", msg);
+			return "errorpage";
+		
+		}
+	}
 	
 //	@RequestMapping("/hey")
 //	public String display(@RequestParam("name") String name,@RequestParam("pass") String pass,Model m) {
